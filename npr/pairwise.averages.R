c8d25c5dd266770f4027dc58c96765374734d680
koho <ihopeme.hd@gmail.com>
2016-12-02 20:20:00 +0800

    Update pairwise.averages.R

--- npr/pairwise.averages.R
+++ npr/pairwise.averages.R
@@ -1,7 +1,7 @@
 pairwise.averages <- function(x, simplify = FALSE) {
   x <- sort(x)
   names(x) <- x
-  avg <- outer(x, x, function(a, b) { (a + b) / 2 })
-  avg[lower.tri(avg)] <- NA
-  if (simplify) avg[upper.tri(avg, diag = TRUE)] else avg
+  avgs <- outer(x, x, `+`) / 2
+  avgs[lower.tri(avgs)] <- NA
+  if (simplify) avgs[!lower.tri(avgs)] else avgs
 }
