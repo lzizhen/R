3070d3d0fbf43baccfcd0712f24fe04c60cd147c
koho <ihopeme.hd@gmail.com>
2017-03-11 19:21:20 +0800

    Update and rename copy to copy.R

--- copy.R
+++ copy.R
@@ -1,6 +1,6 @@
 copy <- function(x, header = FALSE) {
   if (is.vector(x))
-    x = list(x)
+    x <- list(x)
   data <- .Internal(paste(x, '\t', NULL))
   if (header && !is.null(names(x)))
     data <- c(paste(names(x), collapse = '\t'), data)
